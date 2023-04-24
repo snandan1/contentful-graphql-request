662b57b6359eb0ad7f4c612fc7196b834cae2838
Souvik Nandan <souvik.nandan@gmail.com>
2023-04-23 23:06:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# contentful-graphql-request
+ 
---
+++ src/index.js
@@ -0,0 +1,62 @@
+// const name = 'Happy Coding!'
+
+import { GraphQLClient, gql } from 'graphql-request'
+
+async function main() {
+  const endpoint = 
+    'https://graphql.contentful.com/content/v1/spaces/in9qccnw3m3i/environments/master'
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: 'Bearer XXXX',
+    },
+  })
+
+  const query = gql`
+    query postEntryQuery {
+        post(id: "4CYBcZ1rya6dlE67EIzyrP") {
+        title
+        slug
+        coverImage {
+            title
+            url
+        }
+        content {
+            links {
+            entries {
+                block {
+                ... on Author {
+                    name
+                    picture {
+                    url
+                    }
+                }
+                ... on ProductSection {
+                    productsCollection (limit: 10) {
+                    items {
+                        title
+                        slug
+                        categoriesCollection (limit: 5) {
+                        items {
+                            title
+                        }
+                        }
+                    }
+                    }
+                }
+                }
+            }
+            }
+        }
+        }
+    }
+  `
+
+  const data = await graphQLClient.request(query)
+  console.log('title = ' + data.post.title);
+  console.log(JSON.stringify(data, undefined, 2))
+}
+
+main().catch((error) => console.error(error))
+
+// module.exports = name
